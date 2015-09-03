56b48612093529f06369b68ebc0c3bdb89f5a8ba
Stewart Douglas <sdouglas@quantopian.com>
2015-09-03 10:39:06 -0400

    BUG: Avoid plotting unrecorded data.

--- zipline/examples/buyapple_analyze.py
+++ zipline/examples/buyapple_analyze.py
@@ -1,10 +1,19 @@
 import matplotlib.pyplot as plt
+import logbook
+
+log = logbook.Logger('Algorithm')
 
 
 def analyze(context, perf):
     ax1 = plt.subplot(211)
     perf.portfolio_value.plot(ax=ax1)
     ax2 = plt.subplot(212, sharex=ax1)
-    perf.AAPL.plot(ax=ax2)
+    # Check whether AAPL data has been record()ed.
+    # If it has been, then plot it. Otherwise log
+    # fact that it has not been recorded.
+    if 'AAPL' in perf.columns:
+        perf.AAPL.plot(ax=ax2)
+    else:
+        log.info('No data captured using record().')
     plt.gcf().set_size_inches(18, 8)
     plt.show()
--- zipline/examples/dual_moving_average_analyze.py
+++ zipline/examples/dual_moving_average_analyze.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import logbook
+
+log = logbook.Logger('Algorithm')
 
 
 def analyze(context, perf):
@@ -8,17 +11,26 @@ def analyze(context, perf):
     ax1.set_ylabel('portfolio value in $')
 
     ax2 = fig.add_subplot(212)
-    perf['AAPL'].plot(ax=ax2)
-    perf[['short_mavg', 'long_mavg']].plot(ax=ax2)
-
-    perf_trans = perf.ix[[t != [] for t in perf.transactions]]
-    buys = perf_trans.ix[[t[0]['amount'] > 0 for t in perf_trans.transactions]]
-    sells = perf_trans.ix[
-        [t[0]['amount'] < 0 for t in perf_trans.transactions]]
-    ax2.plot(buys.index, perf.short_mavg.ix[buys.index],
-             '^', markersize=10, color='m')
-    ax2.plot(sells.index, perf.short_mavg.ix[sells.index],
-             'v', markersize=10, color='k')
     ax2.set_ylabel('price in $')
-    plt.legend(loc=0)
+
+    # If data has been record()ed, then plot it.
+    # Otherwise, log the fact that no data has been recorded.
+    if 'AAPL' in perf and 'short_mavg' in perf and 'long_mavg' in perf:
+        perf['AAPL'].plot(ax=ax2)
+        perf[['short_mavg', 'long_mavg']].plot(ax=ax2)
+
+        perf_trans = perf.ix[[t != [] for t in perf.transactions]]
+        buys = perf_trans.ix[[t[0]['amount'] > 0 for t in
+                              perf_trans.transactions]]
+        sells = perf_trans.ix[
+            [t[0]['amount'] < 0 for t in perf_trans.transactions]]
+        ax2.plot(buys.index, perf.short_mavg.ix[buys.index],
+                 '^', markersize=10, color='m')
+        ax2.plot(sells.index, perf.short_mavg.ix[sells.index],
+                 'v', markersize=10, color='k')
+        plt.legend(loc=0)
+    else:
+        ax2.annotate('No data captured using record().', xy=(0.35, 0.5))
+        log.info('No data captured using record().')
+
     plt.show()
